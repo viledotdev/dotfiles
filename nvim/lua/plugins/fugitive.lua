9e016644e894b0535a18b78b1e51423e11d47006
V.L. <vleonbar.dev@gmail.com>
2024-09-11 16:02:29 +0200

    Changed fugitive shortcut error, added ssh on zsh.

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -3,9 +3,9 @@ return {
   cmd = { "G", "Git" },
   keys = {
     { "<leader>gf", ":Git fetch --all -p<cr>", desc = "Git fetch" },
-    { "<leader>g<leader>", ":Git ", desc = "Git fetch" },
-    { "<leader>gl", ":Git pull<cr>",           desc = "Git pull" },
-    { "<leader>gaa", ":Git add --all<cr>",      desc = "Git add" },
-    { "<leader>gp", ":Git push<cr>",           desc = "Git push" },
-  }
+    { "<leader>g<leader>", ":Git <cr>", desc = "Open fugitive" },
+    { "<leader>gl", ":Git pull<cr>", desc = "Git pull" },
+    { "<leader>gaa", ":Git add --all<cr>", desc = "Git add" },
+    { "<leader>gp", ":Git push<cr>", desc = "Git push" },
+  },
 }
