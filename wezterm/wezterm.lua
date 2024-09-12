de09cc595d4b5133ec94a4f9de7351b3f4c0a0ae
VLeon-B <vleonbar.dev@gmail.com>
2024-09-12 17:28:55 +0200

    Starship added. Minor changes to wezterm apparience.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,23 +5,44 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local act = wezterm.action
 -- This is where you actually apply your config choices
+config = {
 
--- For example, changing the color scheme:
-config.color_scheme = "HaX0R_BLUE"
-config.font = wezterm.font("RecMonoLinear Nerd Font Mono")
-config.font_size = 14.0
-config.line_height = 1.3
+	window_background_opacity = 1,
+	enable_tab_bar = false,
+	inactive_pane_hsb = {
+		saturation = 0.75,
+		brightness = 0.45,
+	},
+	automatically_reload_config = true,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
+	background = {
+		{
+			source = {
+				Color = "#02051F",
+			},
+			width = "100%",
+			height = "100%",
+			opacity = 0.75,
+		},
+	},
+	-- For example, changing the color scheme:
+	color_scheme = "Tokio Night",
+	font = wezterm.font("RecMonoLinear Nerd Font Mono"),
+	font_size = 22.0,
+	line_height = 1.25,
 
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
-config.keys = {
-	-- Pane config
-	{ key = "|", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
-	{ key = "t", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
-	{ key = "n", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
-	{ key = "s", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
-	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
+	keys = {
+		-- Pane config
+		{ key = "|", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+		{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+		{ key = "t", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+		{ key = "n", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+		{ key = "s", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+		{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+	},
 }
 -- and finally, return the configuration to wezterm
 return config
