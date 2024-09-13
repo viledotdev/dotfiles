3a2d7480ff1de25671bc65bbbb5f6663b0527ca2
VLeon-B <vleonbar.dev@gmail.com>
2024-09-13 15:16:07 +0200

    Aerospace config added.Changes to setup script for MacOS

--- setup.sh
+++ setup.sh
@@ -1,52 +1,67 @@
-DOTFILES_DIR="$(realpath "$(dirname "$0")")"
+#!/bin/bash
 
-#Check if yay is installed or install it
-if ! command -v "yay" &> /dev/null; then
-        echo "Installing yay..."
-        sudo pacman -S --noconfirm "yay"
-    else
-        echo "yay already installed."
-    fi
+if [ "$(basename -- "$SHELL")" != "zsh" ]; then
+    echo "This script must be run in a zsh shell."
+    exit 1
+fi
+
+DOTFILES_DIR="$(realpath "$(dirname "$0")")"
+PACKAGE_MANAGER="brew"
+
+if ! command -v "$PACKAGE_MANAGER" &> /dev/null; then
+    echo "Installing PM -> $PACKAGE_MANAGER..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "PM -> $PACKAGE_MANAGER already installed."
+fi
 
 create_symlink() {
     local target="$(realpath "$1")"
-    local link="$(realpath -m "$2")"
+    local link="$HOME/.config/$(basename "$target")"
 
     if [ ! -e "$link" ]; then
         ln -s "$target" "$link"
         echo "Symlink created: $link -> $target"
     else
-        echo "Symlink already done or config file exists $link"
+        echo "Symlink already done or config file exists: $link"
     fi
 }
 
 install_if_missing() {
     local app="$1"
 
-    # Verify if app is already installed
     if ! command -v "$app" &> /dev/null; then
         echo "Installing $app..."
-        yay -S --noconfirm "$app"
+        $PACKAGE_MANAGER install "$app"
     else
         echo "$app already installed."
     fi
 }
 
+link_zshrc() {
+    ZSHRC_TARGET="$DOTFILES_DIR/zshrc"
+    ZSHRC_LINK="$HOME/.config/.zshrc"
+    
+    if [ ! -L "$ZSHRC_LINK" ]; then
+        ln -s "$ZSHRC_TARGET" "$ZSHRC_LINK"
+        echo "Symlink created: $ZSHRC_LINK -> $ZSHRC_TARGET"
+    else
+        echo "Symlink for zshrc already exists or config file exists: $ZSHRC_LINK"
+    fi
+}
+
+# Main exec:
+
+link_zshrc
+
 for folder in "$DOTFILES_DIR"/*/; do
-    # Obtener el nombre de la carpeta sin la ruta
     app=$(basename "$folder")
 
-    # Excluir carpetas que comiencen con "_"
     if [[ "$app" =~ ^_ ]]; then
         echo "Skipping $app"
         continue
     fi
 
-    # Instalar la aplicación usando el nombre de la carpeta
     install_if_missing "$app"
-
-    # Crear symlink de la carpeta completa en ~/.config
-    create_symlink "$folder" "$HOME/.config/$app"
+    create_symlink "$folder"
 done
-
-
