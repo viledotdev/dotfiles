5c2a045198e65316e86aaa87d8d806bac7f0a69b
VLeon-B <vleonbar.dev@gmail.com>
2024-09-20 19:14:19 +0200

    Sketchybar config added. 1.0 version.

---
+++ sketchybar/items/front_app.lua
@@ -0,0 +1,35 @@
+local colors = require("colors")
+local icon_map = require("icon_map")
+
+local front_app = sbar.add("item", {
+	label = {
+		font = {
+			style = "Black",
+			size = 12.0,
+		},
+	},
+})
+
+front_app:subscribe("front_app_switched", function(env)
+	print(env.INFO)
+	if env.INFO ~= nil then
+		sbar.set(env.NAME, {
+			icon = {
+				font = "sketchybar-app-font:Regular:16.0",
+				color = colors.green,
+				string = icon_map.get_icon(env.INFO),
+				drawing = true,
+				padding_left = 15,
+				padding_right = 15,
+			},
+			padding_left = 5,
+			background = { color = colors.black, border_color = colors.red },
+			padding_right = 5,
+			label = {
+				color = colors.green,
+				padding_right = 15,
+				string = env.INFO,
+			},
+		})
+	end
+end)
