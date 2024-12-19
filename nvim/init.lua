40da0461b0bc6df3eb234101805fea8fb9c3b1c0
VLeon-B <vleonbar.dev@gmail.com>
2024-12-19 21:27:06 +0100

    some test and few things added

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,3 @@
 require("config.settings")
 require("config.lazy")
 require("config.keymaps")
-
--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -2,26 +2,26 @@ return {
   "jiaoshijie/undotree",
   dependencies = "nvim-lua/plenary.nvim",
   opts = {
-    float_diff = true,      -- using float window previews diff, set this `true` will disable layout option
+    float_diff = true, -- using float window previews diff, set this `true` will disable layout option
     layout = "left_bottom", -- "left_bottom", "left_left_bottom"
-    position = "left",      -- "right", "bottom"
-    ignore_filetype = { 'undotree', 'undotreeDiff', 'qf', 'TelescopePrompt', 'spectre_panel', 'tsplayground' },
+    position = "left", -- "right", "bottom"
+    ignore_filetype = { "undotree", "undotreeDiff", "qf", "TelescopePrompt", "spectre_panel", "tsplayground" },
     window = {
       winblend = 30,
     },
     keymaps = {
-      ['j'] = "move_next",
-      ['k'] = "move_prev",
-      ['gj'] = "move2parent",
-      ['J'] = "move_change_next",
-      ['K'] = "move_change_prev",
-      ['<cr>'] = "action_enter",
-      ['p'] = "enter_diffbuf",
-      ['q'] = "quit",
+      ["t"] = "move_next",
+      ["n"] = "move_prev",
+      ["gt"] = "move2parent",
+      ["T"] = "move_change_next",
+      ["N"] = "move_change_prev",
+      ["<cr>"] = "action_enter",
+      ["p"] = "enter_diffbuf",
+      ["q"] = "quit",
     },
   },
   config = function(_, opts)
-    require('undotree').setup(opts)
+    require("undotree").setup(opts)
   end,
   keys = { -- load the plugin only when using it's keybinding:
     { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
