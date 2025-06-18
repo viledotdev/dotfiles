6161456fdbc639e4897233650a91fc704d98c080
viledotconf <vleonbar.dev@gmail.com>
2025-06-18 16:42:34 +0200

    Refactor(wezterm): visual formatting.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,8 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
-local act = wezterm.action
--- This is where you actually apply your config choices
+local action = wezterm.action
+
 local mods = "CTRL | ALT"
 config = {
 
@@ -27,7 +25,6 @@ config = {
 			opacity = 0.75,
 		},
 	},
-	-- For example, changing the color scheme:
 	color_scheme = "Tokio Night",
 	font = wezterm.font("RecMonoLinear Nerd Font Mono"),
 	font_size = 16.0,
@@ -36,14 +33,13 @@ config = {
 	-- leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
 	keys = {
 		-- Pane config
-		{ key = "v", mods = mods, action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-		{ key = "z", mods = mods, action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-		{ key = "h", mods = mods, action = act.ActivatePaneDirection("Left") },
-		{ key = "t", mods = mods, action = act.ActivatePaneDirection("Down") },
-		{ key = "n", mods = mods, action = act.ActivatePaneDirection("Up") },
-		{ key = "s", mods = mods, action = act.ActivatePaneDirection("Right") },
-		{ key = "w", mods = mods, action = act.CloseCurrentPane({ confirm = true }) },
+		{ key = "v", mods = mods, action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "z", mods = mods, action = action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+		{ key = "h", mods = mods, action = action.ActivatePaneDirection("Left") },
+		{ key = "t", mods = mods, action = action.ActivatePaneDirection("Down") },
+		{ key = "n", mods = mods, action = action.ActivatePaneDirection("Up") },
+		{ key = "s", mods = mods, action = action.ActivatePaneDirection("Right") },
+		{ key = "w", mods = mods, action = action.CloseCurrentPane({ confirm = true }) },
 	},
 }
--- and finally, return the configuration to wezterm
 return config
